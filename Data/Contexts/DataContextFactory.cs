9803e23ed57caeebdbfbce7d258c293269ccb84c
Angelica Olsson <angelica.olsson1@learnet.se>
2024-08-08 09:58:12 +0200

    Update

--- Data/Contexts/DataContextFactory.cs
+++ Data/Contexts/DataContextFactory.cs
@@ -8,7 +8,7 @@ internal class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:silicon-sqlserver-hamhol.database.windows.net,1433;Initial Catalog=silicon-database;Persist Security Info=False;User ID=sqladmin;Password=vetinte123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("Server=tcp:sql-win23jsbackend-anggelick.database.windows.net,1433;Initial Catalog=identity_database;Persist Security Info=False;User ID=SqlAdmin;Password=anggelick;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         return new DataContext(optionsBuilder.Options);
     }
